d1d34d721cf85cd6544b485e2fc77e3a4e48615c
Cedric Don Coloma <colomce@oocl.com>
2020-10-18 02:17:46 +0800

    [ParkingLot][Cedric] refactor: Removed unnecessary imports, change access modifier, and used static import.

--- src/test/java/com/oocl/cultivation/ParkingLotTest.java
+++ src/test/java/com/oocl/cultivation/ParkingLotTest.java
@@ -1,14 +1,14 @@
 package com.oocl.cultivation;
 
-import com.sun.webkit.PageCache;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class ParkingLotTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ParkingLotTest {
 
     @Test
-    public void should_return_capacity_10_when_parking_lot_is_created_given_no_capacity() {
+    void should_return_capacity_10_when_parking_lot_is_created_given_no_capacity() {
         ParkingLot parkingLot = new ParkingLot();
-        Assertions.assertEquals(10 , parkingLot.getCapacity());
+        assertEquals(10, parkingLot.getCapacity());
     }
 }
