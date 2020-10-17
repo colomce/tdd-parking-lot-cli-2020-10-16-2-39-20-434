da0b9dcd4b66840f834dcc0943fd04fa93165868
Cedric Don Coloma <colomce@oocl.com>
2020-10-18 02:23:34 +0800

    [ParkingLot][Cedric] test: Added test for ParkingBoy to accept a parking lot.

--- src/main/java/com/oocl/cultivation/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -1,4 +1,11 @@
 package com.oocl.cultivation;
 
 public class ParkingBoy {
+    public ParkingBoy(ParkingLot parkingLot) {
+
+    }
+
+    public ParkingLot getParkingLot() {
+        return null;
+    }
 }
--- src/test/java/com/oocl/cultivation/ParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/ParkingBoyTest.java
@@ -2,9 +2,20 @@ package com.oocl.cultivation;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 class ParkingBoyTest {
     @Test
-    void write_your_first_test() {
+    void should_return_parking_lot_when_parking_boy_is_created_given_parking_lot() {
+        //given
+        ParkingBoy parkingBoy = new ParkingBoy(new ParkingLot());
 
+        //when
+        ParkingLot parkingLot = parkingBoy.getParkingLot();
+
+        //then
+        assertNotNull(parkingLot);
+        assertEquals(ParkingLot.class, parkingLot.getClass());
     }
 }
