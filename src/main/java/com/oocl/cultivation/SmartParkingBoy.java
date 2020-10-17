07dee231821cf2018c7b9c673778728613834962
Cedric Don Coloma <colomce@oocl.com>
2020-10-18 06:11:51 +0800

    [ParkingLot][Cedric] feature: Added implementation SmartParkingBoy can park a car in first ParkingLot

--- src/main/java/com/oocl/cultivation/SmartParkingBoy.java
+++ src/main/java/com/oocl/cultivation/SmartParkingBoy.java
@@ -3,10 +3,13 @@ package com.oocl.cultivation;
 import java.util.List;
 
 public class SmartParkingBoy {
+    private List<ParkingLot> parkingLots;
+
     public SmartParkingBoy(List<ParkingLot> parkingLots) {
+        this.parkingLots = parkingLots;
     }
 
     public ParkingTicket park(Car car) {
-        return null;
+        return parkingLots.get(0).park(car);
     }
 }
