643e4c4a25062ef78d24ec634f3068d1fc394f44
Cedric Don Coloma <colomce@oocl.com>
2020-10-18 06:10:52 +0800

    [ParkingLot][Cedric] test: Added test to check if SmartParkingBoy can park a car in first ParkingLot

---
+++ src/main/java/com/oocl/cultivation/SmartParkingBoy.java
@@ -0,0 +1,12 @@
+package com.oocl.cultivation;
+
+import java.util.List;
+
+public class SmartParkingBoy {
+    public SmartParkingBoy(List<ParkingLot> parkingLots) {
+    }
+
+    public ParkingTicket park(Car car) {
+        return null;
+    }
+}
---
+++ src/test/java/com/oocl/cultivation/SmartParkingBoyTest.java
@@ -0,0 +1,26 @@
+package com.oocl.cultivation;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static java.util.Arrays.asList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SmartParkingBoyTest {
+    @Test
+    void should_first_parking_lot_have_one_parked_car_when_smart_parking_boy_park_a_car_given_two_parking_lots_with_no_parked_cars() {
+        //given
+        ParkingLot firstParkingLot = new ParkingLot(5);
+        ParkingLot secondParkingLot = new ParkingLot(5);
+        List<ParkingLot> parkingLots = asList(firstParkingLot, secondParkingLot);
+        Car car = new Car();
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLots);
+
+        //when
+        ParkingTicket parkingTicket = smartParkingBoy.park(car);
+
+        //then
+        assertEquals(1, firstParkingLot.getParkingTickets().size());
+    }
+}
