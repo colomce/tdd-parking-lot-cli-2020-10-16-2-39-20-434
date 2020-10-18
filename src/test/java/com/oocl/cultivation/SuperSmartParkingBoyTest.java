8497c10397fdcd73698acea6b805724a5fe746a4
Cedric Don Coloma <colomce@oocl.com>
2020-10-18 08:08:27 +0800

    [ParkingLot][Cedric] refactor:  Refactor test name

--- src/test/java/com/oocl/cultivation/SuperSmartParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/SuperSmartParkingBoyTest.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class SuperSmartParkingBoyTest {
     @Test
-    void should_return_parking_ticket_when_smart_parking_boy_parks_a_car_given_parking_lot_with_1_capacity() {
+    void should_return_parking_ticket_when_super_mart_parking_boy_parks_a_car_given_parking_lot_with_1_capacity() {
         //given
         ParkingLot parkingLot = new ParkingLot(1);
         List<ParkingLot> parkingLots = asList(parkingLot);
