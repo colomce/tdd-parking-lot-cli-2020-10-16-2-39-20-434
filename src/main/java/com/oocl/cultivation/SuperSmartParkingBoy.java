94cfab7299d7ada83d1f767c9a3dfeda973f268e
Cedric Don Coloma <colomce@oocl.com>
2020-10-18 08:03:04 +0800

    [ParkingLot][Cedric] test: Added implementation to for Super Smart Parking Boy to park a single car

---
+++ src/main/java/com/oocl/cultivation/SuperSmartParkingBoy.java
@@ -0,0 +1,13 @@
+package com.oocl.cultivation;
+
+import java.util.List;
+
+public class SuperSmartParkingBoy {
+    public SuperSmartParkingBoy(List<ParkingLot> parkingLots) {
+
+    }
+
+    public ParkingTicket park(Car car) {
+        return null;
+    }
+}
---
+++ src/test/java/com/oocl/cultivation/SuperSmartParkingBoyTest.java
@@ -0,0 +1,25 @@
+package com.oocl.cultivation;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static java.util.Arrays.asList;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class SuperSmartParkingBoyTest {
+    @Test
+    void should_return_parking_ticket_when_smart_parking_boy_parks_a_car_given_parking_lot_with_1_capacity() {
+        //given
+        ParkingLot parkingLot = new ParkingLot(1);
+        List<ParkingLot> parkingLots = asList(parkingLot);
+        Car car = new Car();
+        SuperSmartParkingBoy superSmartParkingBoy = new SuperSmartParkingBoy(parkingLots);
+
+        //when
+        ParkingTicket parkingTicket = superSmartParkingBoy.park(car);
+
+        //then
+        assertNotNull(parkingTicket);
+    }
+}
