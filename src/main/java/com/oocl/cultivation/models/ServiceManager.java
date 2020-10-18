9b41faaab5d8ab7feeb4620e1f5c333cc8588836
Cedric Don Coloma <colomce@oocl.com>
2020-10-18 12:33:07 +0800

    [ParkingLot][Cedric] feature: Added implementation so that Manager can park and fetch the right car as Normal Parking Behavior and Fetching Behavior

--- src/main/java/com/oocl/cultivation/models/ServiceManager.java
+++ src/main/java/com/oocl/cultivation/models/ServiceManager.java
@@ -1,16 +1,36 @@
 package com.oocl.cultivation.models;
 
+import com.oocl.cultivation.behaviors.FetchingBehavior;
+import com.oocl.cultivation.behaviors.NormalParkingBehavior;
+
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
 public class ServiceManager extends ParkingLotEmployee {
+    private FetchingBehavior fetchingBehavior;
+    private NormalParkingBehavior normalParkingBehavior;
+    private List<ParkingLot> parkingLots;
+
     public ServiceManager(ParkingLot parkingLot) {
+        this.parkingLots = asList(parkingLot);
+        fetchingBehavior = new FetchingBehavior(this.parkingLots);
+        normalParkingBehavior = new NormalParkingBehavior(this.parkingLots);
+    }
+
+    public ServiceManager(List<ParkingLot> parkingLots) {
+        this.parkingLots = parkingLots;
+        fetchingBehavior = new FetchingBehavior(this.parkingLots);
+        normalParkingBehavior = new NormalParkingBehavior(this.parkingLots);
     }
 
     @Override
     public ParkingTicket park(Car car) {
-        return null;
+        return normalParkingBehavior.park(car);
     }
 
     @Override
     public Car fetch(ParkingTicket parkingTicket) {
-        return null;
+        return fetchingBehavior.fetch(parkingTicket);
     }
 }
