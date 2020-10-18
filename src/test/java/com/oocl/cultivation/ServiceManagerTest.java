5d03c5ad6f9a06bc8223c74b7ccc29ac6a5ab1fa
Cedric Don Coloma <colomce@oocl.com>
2020-10-18 12:40:23 +0800

    [ParkingLot][Cedric] test: Added test case to check if Manager can park and fetch the right car as Normal Parking Behavior and FetchingBehavior when there are multiple parking lots

--- src/test/java/com/oocl/cultivation/ServiceManagerTest.java
+++ src/test/java/com/oocl/cultivation/ServiceManagerTest.java
@@ -6,6 +6,9 @@ import com.oocl.cultivation.models.ParkingTicket;
 import com.oocl.cultivation.models.ServiceManager;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertSame;
 
 public class ServiceManagerTest {
@@ -21,6 +24,27 @@ public class ServiceManagerTest {
         //when
         Car fetchedFirstCar = serviceManager.fetch(firstParkingTicket);
         Car fetchedSecondCar = serviceManager.fetch(secondParkingTicket);
+
+        //then
+        assertSame(firstCar, fetchedFirstCar);
+        assertSame(secondCar, fetchedSecondCar);
+    }
+
+    @Test
+    void should_fetch_two_right_cars_when_manager_parking_boy_fetch_given_two_parked_cars_two_parking_lot_and_parking_tickets() {
+        //given
+        ParkingLot firstParkingLot = new ParkingLot(1);
+        ParkingLot secondParkingLot = new ParkingLot(1);
+        Car firstCar = new Car();
+        Car secondCar = new Car();
+        List<ParkingLot> parkingLots = Arrays.asList(firstParkingLot, secondParkingLot);
+        ServiceManager serviceManager = new ServiceManager(parkingLots);
+        ParkingTicket firstCarParkingTicket = serviceManager.park(firstCar);
+        ParkingTicket secondCarParkingTicket = serviceManager.park(secondCar);
+
+        //when
+        Car fetchedFirstCar = serviceManager.fetch(firstCarParkingTicket);
+        Car fetchedSecondCar = serviceManager.fetch(secondCarParkingTicket);
 
         //then
         assertSame(firstCar, fetchedFirstCar);
