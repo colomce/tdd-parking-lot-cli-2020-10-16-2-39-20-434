90401aa9641fdc7f1c9102bd3f754eaef00374ce
Cedric Don Coloma <colomce@oocl.com>
2020-10-19 18:10:48 +0800

    [ParkingLot][Cedric] refactor: Place constants on top of constructor

--- src/main/java/com/oocl/cultivation/constants/ParkingLotConstants.java
+++ src/main/java/com/oocl/cultivation/constants/ParkingLotConstants.java
@@ -1,10 +1,10 @@
 package com.oocl.cultivation.constants;
 
 public final class ParkingLotConstants {
-    private ParkingLotConstants() {
-    }
-
     public static final String NOT_ENOUGH_POSITION_MSG = "Not enough position";
     public static final String UNRECOGNIZED_PARKING_TICKET_MSG = "Unrecognized parking ticket";
     public static final String PROVIDE_YOUR_TICKET_MSG = "Please provide your parking ticket";
+    
+    private ParkingLotConstants() {
+    }
 }
